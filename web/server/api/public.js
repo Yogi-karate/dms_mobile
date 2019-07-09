e9511db74a0452a17d4beac90d8d803e46a3ba90
tramm <ram.tangirala@gmail.com>
2019-07-09 17:14:34 +0530

    test public api for cloud telephony

--- web/server/api/public.js
+++ web/server/api/public.js
@@ -5,4 +5,13 @@ const router = express.Router();
 router.use((req, res, next) => {
   next();
 });
+router.get('/send', async (req, res) => {
+  try {
+    let result = {};
+    console.log("THE PUBLIC Request Send ---> ",req);
+    res.json(result);
+  } catch (err) {
+    res.status(200).send({ message: "thanks for reaching out !!!"});
+  }
+});
 module.exports = router;
