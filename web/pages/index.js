297296d79f48a375998ad5d0ea0d7235c75c314e
Karthik Gopalan <karthik@saboo.in>
2019-07-22 19:04:21 +0530

    added changes to index.js for css files

--- web/pages/index.js
+++ web/pages/index.js
@@ -7,7 +7,11 @@ import Head from 'next/head';
 import withAuth from '../lib/withAuth';
 import withLayout from '../lib/withLayout';
 import dynamic from 'next/dynamic'
+
+import DMSManagerDashboard from './DMSManagerDashboard.jsx';
+
 const Dashboard = dynamic(import('../components/dashboard'), { ssr: false })
+  
 //import Dashboard from '../components/dashboard';
 
 class Index extends React.Component {
@@ -27,14 +31,16 @@ class Index extends React.Component {
       <div style={{ padding: '10px 45px' }}>
         <Head>
           <title>Dashboard</title>
+          <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css"/>
           <meta name="description" content="description for indexing bots" />
         </Head>
         <p> Dashboard </p>
-        <Dashboard {...this.props} />
-        <p>Email: {user.email}</p>
+        <DMSManagerDashboard {...this.props} />
+        {/* <p>Email: {user.email}</p> */}
       </div>
     );
   }
 }
 
-export default withAuth(withLayout(Index));
+// export default withAuth(withLayout(Index));
+export default withLayout(Index);
