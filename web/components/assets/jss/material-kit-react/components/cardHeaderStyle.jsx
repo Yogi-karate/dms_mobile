3431ec546593c2b0517f37388fc6bb272205fd17
Karthik Gopalan <karthik@saboo.in>
2019-07-22 19:16:18 +0530

    Added dependencies

--- web/components/assets/jss/material-kit-react/components/cardHeaderStyle.jsx
+++ web/components/assets/jss/material-kit-react/components/cardHeaderStyle.jsx
@@ -3,7 +3,8 @@ import {
   successCardHeader,
   dangerCardHeader,
   infoCardHeader,
-  primaryCardHeader
+  primaryCardHeader,
+  followUpCardHeader
 } from "../../material-kit-react.jsx";
 const cardHeaderStyle = {
   cardHeader: {
@@ -23,7 +24,8 @@ const cardHeaderStyle = {
   successCardHeader,
   dangerCardHeader,
   infoCardHeader,
-  primaryCardHeader
+  primaryCardHeader,
+  followUpCardHeader
 };
 
 export default cardHeaderStyle;
