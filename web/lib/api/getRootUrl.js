8428d9c8026961fa6cbedafc6e65a8cc1822f8be
tramm <ram.tangirala@gmail.com>
2019-08-03 15:15:39 +0530

    reverted testing in leads api and env changes for multi environment

--- web/lib/api/getRootUrl.js
+++ web/lib/api/getRootUrl.js
@@ -2,11 +2,12 @@ import getConfig from 'next/config'
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
 
 export default function getRootUrl() {
-  console.log(publicRuntimeConfig.host) // Will only be available on the server side
+  
   console.log(publicRuntimeConfig.port) // Will be available on both server and client
   const port = publicRuntimeConfig.port || 8001;
   const dev = publicRuntimeConfig.env !== 'production';
   const host = publicRuntimeConfig.mode !='dev' ? publicRuntimeConfig.phost:publicRuntimeConfig.dhost
+  console.log("the host for api is ",host) // Will only be available on the server side
   const ROOT_URL = dev ? `http://localhost:${port}` : host;
   return ROOT_URL;
 }
