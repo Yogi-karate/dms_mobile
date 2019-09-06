854c6db6ca72ebc45a782c1a5d93798d816ef7fb
Anish konda <anishkonda123@gmail.com>
2019-09-06 19:48:16 +0530

    first commit in 1.5

--- client/app.js
+++ client/app.js
@@ -1,7 +1,11 @@
 const notificationApi = require('./notification');
 const serviceLeadSmsApi = require('./serviceLeadSms');
 const insuranceLeadSmsApi = require('./insuranceLeadSms');
+const insuranceBookingSmsApi = require('./insuranceBookingSms');
+const serviceBookingSmsApi = require('./serviceBookingSms');
 
 notificationApi();
 serviceLeadSmsApi();
 insuranceLeadSmsApi();
+serviceBookingSmsApi();
+insuranceBookingSmsApi();
