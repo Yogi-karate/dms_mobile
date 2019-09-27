f36205935516b7204e3e37b3b144fe706b0ef4a2
Anish konda <anishkonda123@gmail.com>
2019-09-27 15:58:24 +0530

    added consoles for executeSMS method in smsJobs js

--- web/server/models/smsJobs.js
+++ web/server/models/smsJobs.js
@@ -118,7 +118,9 @@ class Jobs {
             let newJobLogs = await JobLog.add(NewJobLog);
             let templateString = jobMaster[0].msgTemplate.value;
             let result = await this[`execute${jobMaster[0].action}`](user);
+            console.log("Inside executeSMS result count is ",result.records.length);
             if (result && result.records) {
+                console.log("records are present of length",result.records.length);
                 for (let i = 0; i < result.records.length; i++) {
                     let record = result.records[i];
                     let message = '';
