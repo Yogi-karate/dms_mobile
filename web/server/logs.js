47945e7aa0e0703b195b296410e9d94c59773057
tramm <ram.tangirala@gmail.com>
2020-01-29 20:52:42 +0530

    logging changes and serverless addition

--- web/server/logs.js
+++ web/server/logs.js
@@ -1,9 +1,18 @@
 const winston = require('winston');
-
+const { combine, timestamp, label, prettyPrint } = winston.format;
 const logger = winston.createLogger({
   level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-  format: winston.format.combine(winston.format.splat(), winston.format.simple()),
-  transports: [new winston.transports.Console()],
+  format: winston.format.combine(timestamp() , prettyPrint()),
+  transports: [new winston.transports.Console(), new winston.transports.File({
+    level: 'info',
+    filename: '/Users/tramm/dev/projects/react/dms/web/logs/all-logs.log',
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880, //5MB
+    maxFiles: 5,
+    colorize: false
+})],
 });
- 
+
+
 module.exports = logger;
