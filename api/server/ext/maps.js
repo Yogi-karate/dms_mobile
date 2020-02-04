90b7f3638b91697820e661e797adca9f778164ae
tramm <ram.tangirala@gmail.com>
2020-02-04 14:29:44 +0530

    changes to maps distance api

--- api/server/ext/maps.js
+++ api/server/ext/maps.js
@@ -1,16 +1,26 @@
 // Create client with a Promise constructor
+const utils = require('util');
 const googleMapsClient = require('@google/maps').createClient({
-    key: 'AIzaSyBPw139e5-pqPnoOr25Twfr1ZPmZT72JRw',
-    Promise: Promise // 'Promise' is the native constructor.
-  });
-  const marker1 =  googleMapsClient.LatLng(40.714, -74.006);
-  const marker2 =  googleMapsClient.LatLng(46.26, -126.3);
-  console.log("the markers",marker1,marker2);
-  // Geocode an address with a promise
-  googleMapsClient.geocode({address: '1600 Amphitheatre Parkway, Mountain View, CA'}).asPromise()
-    .then((response) => {
-      console.log(response.json.results);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  key: 'AIzaSyBPw139e5-pqPnoOr25Twfr1ZPmZT72JRw',
+  Promise: Promise // 'Promise' is the native constructor.
+});
+const coordinates = {
+  origins: {
+    lat: '6.535578',
+    long: '3.368550'
+  },
+  destinations: {
+    lat: '6.535578',
+    long: '3.368550'
+  }
+}
+googleMapsClient.distanceMatrix({
+  origins: `${coordinates.origins.lat},${coordinates.origins.long}`,
+  destinations: `${coordinates.destinations.lat},${coordinates.destinations.long}`,
+  mode: 'driving' //other mode include "walking" , "bicycling", "transit"
+}).asPromise().then((response) => {
+  console.log(response)
+  let rows = response.json.rows
+  console.log(utils.inspect(rows[0].elements[0]));
+})
+  .catch(err => console.log(err));
