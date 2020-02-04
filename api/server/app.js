1678f39bcbd8c24718125fed02f918aa51135668
Anish0807 <anishkonda123@gmail.com>
2020-02-04 17:10:01 +0530

    added json limit in APP JS

--- api/server/app.js
+++ api/server/app.js
@@ -31,7 +31,10 @@ const URL_MAP = {
 
 const server = express();
 
-server.use(express.json());
+//server.use(express.json());
+server.use(express.json({limit: '50mb'}));
+server.use(express.urlencoded({limit: '50mb'}));
+
 logger.stream = {
     write: function (message, encoding) {
         logger.info(message);
